40d81af86863df5c8c57dcf6de320aed1ab48f40
Ramirezciani <pa.ramirezciani13@gmail.com>
2025-05-13 15:29:55 -0400

    fix origins allow

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,56 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-require("dotenv").config();
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // app.enableCors({
+
+  //   origin: 'http://127.0.0.1:5500', // Ajusta la URL del frontend si es necesario
+  //   methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE', 'PATCH'],
+  //   allowedHeaders: ['Content-Type', 'Authorization'],
+  //   credentials: true,
+  // });
+
   app.enableCors({
-    origin: '', // La URL del frontend, ajústala si es diferente
-    methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE', 'PATCH'], // Métodos permitidos
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true); // Postman, curl, etc.
+
+      const allowedPrefix = 'http://172.25.100.';
+      if (origin.startsWith(allowedPrefix)) {
+        return callback(null, true);
+      }
+
+      if (origin.startsWith('http://localhost')) {
+        return callback(null, true);
+      }
+
+      return callback(new Error('CORS not allowed'), false);
+    },
+    methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(process.env.PORT ?? 3000);
+
+  // swagger config
+  const config = new DocumentBuilder()
+    .setTitle('API TUEMPLEO.CL')
+    .setDescription('Backend para tuempleo.cl')
+    .setVersion('1.0')
+    .build();
+
+  // swagger docs
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const port = parseInt(process.env.PORT ?? '3000', 10);
+  await app.listen(port);
+
 }
+
 void bootstrap();
