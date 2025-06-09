624c3a235e02d1712fe7afbe8c6ee0521dca8609
Ramirezciani <pa.ramirezciani13@gmail.com>
2025-06-09 10:45:25 -0400

    get employer by id

--- src/modules/curriculum/curriculum.controller.ts
+++ src/modules/curriculum/curriculum.controller.ts
@@ -130,5 +130,7 @@ export class CurriculumController {
     };
   }
 
+  
+
 
 }
--- src/modules/employer/employer.controller.ts
+++ src/modules/employer/employer.controller.ts
@@ -1,11 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get, Param } from '@nestjs/common';
 import { EmpleadorService } from './employer.service';
 import { Empleador } from 'src/repository/employer/employer.entity';
 import { CreateEmployerDto } from '../employer/dto/create-employer.dto';
 
 @Controller('v1/empleador')
 export class EmpleadorController {
-  constructor(private readonly empleadorService: EmpleadorService) {}
+  constructor(private readonly empleadorService: EmpleadorService) { }
 
   @Post()
   async createEmployer(@Body() createEmployerDto: CreateEmployerDto): Promise<Empleador> {
@@ -13,5 +13,10 @@ export class EmpleadorController {
       createEmployerDto,
       createEmployerDto.empresaId
     );
+  }
+
+  @Get(':userId')
+  async getEmployerByUserId(@Param('userId') userId: number): Promise<Empleador | null> {
+    return this.empleadorService.findEmployerByUserId(userId);
   }
 }
