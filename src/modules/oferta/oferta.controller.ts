d6e0e6a64e9204d426fa7cf7fae20e39409144a3
Ramirezciani <pa.ramirezciani13@gmail.com>
2025-06-17 16:49:58 -0400

    get ofertas paginadas por empleador

--- src/modules/oferta/oferta.controller.ts
+++ src/modules/oferta/oferta.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Post, Body, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
 import { OfertaService } from './oferta.service';
 import { CreateOfertaDto } from './dto/create-oferta.dto';
 import { Oferta } from '../../repository/job_offer/job-offer.entity';
+import { PageOptionsDto } from 'src/shared/pagination/page-options.dto';
+import { PageDto } from 'src/shared/pagination/page.dto';
 
 @Controller('v1/ofertas')
 export class OfertaController {
@@ -21,5 +23,17 @@ export class OfertaController {
     @Get(':id')
     async obtenerOfertaPorId(@Param('id', ParseIntPipe) id: number) {
         return this.ofertaService.obtenerOfertaPorId(id);
+    }
+
+    // @Get('empleador/:empleadorId')
+    // async obtenerOfertasPorEmpleador(@Param('empleadorId', ParseIntPipe) empleadorId: number): Promise<Oferta[]> {
+    //     return this.ofertaService.obtenerOfertasPorEmpleador(empleadorId);
+    // }
+    @Get('empleador/:empleadorId')
+    async obtenerOfertasPorEmpleador(
+        @Param('empleadorId') empleadorId: number,
+        @Query() pageOptionsDto: PageOptionsDto
+    ): Promise<PageDto<Oferta>> {
+        return this.ofertaService.obtenerOfertasPorEmpleador(empleadorId, pageOptionsDto);
     }
 }
--- src/modules/oferta/oferta.service.ts
+++ src/modules/oferta/oferta.service.ts
@@ -5,6 +5,9 @@ import { Oferta } from "../../repository/job_offer/job-offer.entity";
 import { Empleador } from "../../repository/employer/employer.entity";
 import { Empresa } from "../../repository/business/business.entity";
 import { CreateOfertaDto } from "./dto/create-oferta.dto";
+import { PageOptionsDto } from "src/shared/pagination/page-options.dto";
+import { PageDto } from "src/shared/pagination/page.dto";
+import { PageMetaDto } from "src/shared/pagination/page-meta.dto";
 
 @Injectable()
 export class OfertaService {
@@ -47,4 +50,38 @@ export class OfertaService {
         }
         return oferta;
     }
+
+
+    // async obtenerOfertasPorEmpleador(empleadorId: number): Promise<Oferta[]> {
+    //     const empleador = await this.empleadorRepository.findOne({ where: { id: empleadorId } });
+    //     if (!empleador) {
+    //         throw new NotFoundException(`Empleador con ID ${empleadorId} no encontrado`);
+    //     }
+    //     return this.ofertaRepository.find({
+    //         where: { empleador: { id: empleadorId } },
+    //         relations: ['empresa', 'empleador'],
+    //     });
+    // }
+
+    async obtenerOfertasPorEmpleador(
+        empleadorId: number,
+        pageOptions: PageOptionsDto
+    ): Promise<PageDto<Oferta>> {
+        const queryBuilder = this.ofertaRepository.createQueryBuilder('oferta')
+            .leftJoinAndSelect('oferta.empresa', 'empresa')
+            .leftJoinAndSelect('oferta.empleador', 'empleador')
+            .where('empleador.id = :empleadorId', { empleadorId })
+            .skip(pageOptions.skip)
+            .take(pageOptions.take);
+
+        const [entities, itemCount] = await queryBuilder.getManyAndCount();
+
+        const meta = new PageMetaDto({
+            pageOptionsDto: pageOptions,
+            itemCount,
+        });
+
+        return new PageDto(entities, meta);
+    }
+
 }
