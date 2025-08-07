d30ec5350da1491f0b1ead2785a11cc8c240874e
Paulo Andrés Ramirez Ciani <90012935+Ramirezciani@users.noreply.github.com>
2025-08-07 15:33:18 -0400

    Feature/planes+ (#48)

--- src/modules/webpay+/config/webpay.config.ts
+++ src/modules/webpay+/config/webpay.config.ts
@@ -6,3 +6,11 @@ export const WEBPAY_CONFIG = {
     returnUrl: "https://tuempleo.cl/api/v1/webpay/return",
     finalUrl: "https://www.tuempleo.cl/empresas/resultado-transaccion.html",
 };
+
+// export const WEBPAY_CONFIG = {
+//     commerceCode: "597055555532",
+//     apiKey: "579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C",
+//     environment: "INTEGRACION",
+//     returnUrl: "http://localhost:3000/v1/webpay/return",  // backend local
+//     finalUrl: "http://127.0.0.1:5500/jobox/empresas/resultado-transaccion.html",  // frontend local
+//};
