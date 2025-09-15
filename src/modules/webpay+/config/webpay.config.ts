1af3f7b74622d7636e2a27048a41bf24e854a95e
notbetha <bethapunto@gmail.com>
2025-09-15 09:30:51 -0300

    changes to carrito

--- src/modules/webpay+/config/webpay.config.ts
+++ src/modules/webpay+/config/webpay.config.ts
@@ -1,4 +1,3 @@
-//src/modules/webpay+/config/webpay.config.ts
 export const WEBPAY_CONFIG = {
     commerceCode: "597055555532",
     apiKey: "579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C",
